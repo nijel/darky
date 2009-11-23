f41e39db21f7938c73058d61eae23f26b881c1bf
Michal Čihař <michal@cihar.com>
2009-11-23 20:00:43 +0100

    Initial model.

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from django.utils.translation import ugettext_lazy
+
+import markdown
+
+class Present(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(
+        max_length = 250
+        )
+    description = models.TextField(
+        help_text = ugettext_lazy('Description, you can use markdown.')
+        )
+    description_html = models.TextField(
+        editable = False,
+        blank = True
+        )
+    url = models.URLField()
+
+    def save(self):
+        self.description_html = markdown.markdown(self.description)
+        super(Category, self).save()
