6c063a7b8e815fbfcd29e26e226294c8864eef55
Michal Čihař <michal@cihar.com>
2009-12-01 20:21:00 +0100

    Add priority choice.

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,6 +6,13 @@ from django.utils.translation import ugettext_lazy
 
 import markdown
 
+PRIORITY_CHOICES = (
+    (1, ugettext_lazy('Very low')),
+    (2, ugettext_lazy('Low')),
+    (3, ugettext_lazy('Standard')),
+    (4, ugettext_lazy('High')),
+    (5, ugettext_lazy('Very high')),
+    )
 class Gift(models.Model):
     owner = models.ForeignKey(User, related_name = 'present_set')
     buyer = models.ForeignKey(User, null = True, blank = True, related_name = 'given_set')
@@ -22,6 +29,7 @@ class Gift(models.Model):
         )
     url = models.URLField(null = True, blank = True)
     price = models.IntegerField(null = True, blank = True)
+    priority = models.IntegerField(choices = PRIORITY_CHOICES, default = 3)
 
     def __unicode__(self):
         return '%s (%s)' % (self.title, self.owner.get_full_name())
