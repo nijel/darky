2d62fca283acf72c7ff645d6b645f7281e861f6c
Michal Čihař <michal@cihar.com>
2009-12-01 20:21:40 +0100

    Add gift form.

---
+++ wishlist/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm, Form
+from django.db import models
+from django import forms
+
+from wishlist.models import Gift
+
+from django.utils.translation import ugettext_lazy
+from django.utils.safestring import mark_safe
+
+class NewGift(ModelForm):
+
+    class Meta:
+        model = Phone
+        fields = (
+            'title',
+            'description',
+            'url',
+            'price',
+            'priority',
+            )
