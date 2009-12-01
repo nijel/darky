77d3bd268da77d8607bf955200723ff1ffff6476
Michal Čihař <michal@cihar.com>
2009-12-01 20:15:35 +0100

    Improve admin interface.

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,4 +1,8 @@
 from wishlist.models import Gift
 from django.contrib import admin
 
-admin.site.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'buyer', 'url', 'price')
+    list_filter = ('owner', 'buyer')
+
+admin.site.register(Gift, GiftAdmin)
