9cd4ba504cf10c36cc9bb8c1feb151a0b483e41c
Michal Čihař <michal@cihar.com>
2009-12-01 21:00:52 +0100

    Implement form saving.

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.db.models import Q
 from django.utils.translation import ugettext as _
 from django.template import RequestContext
+
+from django.contrib.auth.models import User
 
 from wishlist.models import Gift
 from wishlist.forms import NewGift
@@ -12,16 +14,23 @@ from wishlist.messages import msg_ok, msg_err
 def overview(request):
     return render_to_response('overview.html', RequestContext(request))
 
-def gift(request, giftid):
+def userlist(request, userid):
+    gift = get_object_or_404(User, username = userid)
+    return render_to_response('gift.html', RequestContext(request, {'gift': gift}))
+
+def gift(request, userid, giftid):
     gift = get_object_or_404(Gift, pk = int(giftid))
+    if gift.user.username != userid:
+        raise Http404('User on gift do not match!')
     return render_to_response('gift.html', RequestContext(request, {'gift': gift}))
 
 def create(request):
     if request.method == 'POST': # If the form has been submitted...
         form = NewGift(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
+            gift = form.save(commit=False)
+            gift.owner = request.user
+            gift.save()
             msg_ok(request, _('Gift created'))
             return HttpResponseRedirect('/') # Redirect after POST
         msg_err(request, _('Gift not created!'))
