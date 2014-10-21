c8fe8271f7faceab0a8b7c3c66622acd7ee58841
Michal Čihař <michal@cihar.com>
2014-10-21 10:27:19 +0200

    Migrations
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ wishlist/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Gift',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=250, verbose_name='Title')),
+                ('description', models.TextField(help_text='You can use <a href="http://daringfireball.net/projects/markdown/syntax">markdown</a>.', verbose_name='Description', blank=True)),
+                ('description_html', models.TextField(editable=False, blank=True)),
+                ('url', models.URLField(null=True, verbose_name='Link', blank=True)),
+                ('price', models.IntegerField(null=True, verbose_name='Expected price', blank=True)),
+                ('priority', models.IntegerField(default=3, verbose_name='Priority', choices=[(1, 'Very low'), (2, 'Low'), (3, 'Standard'), (4, 'High'), (5, 'Very high')])),
+                ('buyer', models.ForeignKey(related_name=b'given_set', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('owner', models.ForeignKey(related_name=b'present_set', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
