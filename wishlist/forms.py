70d3782f96e2725e1b501e401d2573e2cecc31ab
Michal Čihař <michal@cihar.com>
2014-10-23 11:22:32 +0200

    Remove unused imports
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -1,15 +1,10 @@
-from django.forms import ModelForm, Form
-from django.db import models
-from django import forms
+from django.forms import ModelForm
 
 from wishlist.models import Gift
 
-from django.utils.translation import ugettext_lazy
-from django.utils.safestring import mark_safe
 
 class NewGift(ModelForm):
-
-    class Meta:
+    class Meta(object):
         model = Gift
         fields = (
             'title',
